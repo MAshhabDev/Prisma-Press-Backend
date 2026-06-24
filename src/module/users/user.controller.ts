343d573720f062a0342c99fcccb6e5084ad539e4
Mahir4098 <mahirjr98@gmail.com>
2026-06-24 13:56:12 +0600

    Implement Send Response function to increase code reusability

--- src/module/users/user.controller.ts
+++ src/module/users/user.controller.ts
@@ -3,14 +3,13 @@ import type { NextFunction, Request, RequestHandler, Response } from "express";
 import httpStatus from "http-status";
 import { userService } from "./user.service";
 import { catchAsync } from "../../utils/catchAsync";
-
-
+import { sendResponse } from "../../utils/sendResponse";
 
 const createUser = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const result = await userService.createUserInToDb(req.body);
 
-    res.status(httpStatus.CREATED).json({
+    sendResponse(res, {
       success: true,
       statusCode: httpStatus.CREATED,
       message: "User registered successfully",
